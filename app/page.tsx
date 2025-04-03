688387e1cc646a7931dff179a714c2c6ee075938
mihreteab-bizuayehu <lijmihret@gmail.com>
2025-04-04 02:04:25 +0300

    Type update

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,32 @@
-import { getPosts } from "./actions";
-import { Suspense } from "react";
-import BlogPost from "@/components/generals/BlogPost";
+import { getPosts } from './actions';
+import { Suspense } from 'react';
+import BlogPost from '@/components/generals/BlogPost';
 
- export default function Home() {
-  
+export default function Home() {
   return (
-    <Suspense fallback={<div className="flex items-center justify-center h-screen text-2xl text-blue-500 font-extrabold">Loading...</div>}>
-      <BlogPosts/>
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen text-2xl text-blue-500 font-extrabold">
+          Loading...
+        </div>
+      }
+    >
+      <BlogPostList />
     </Suspense>
-  )
- 
+  );
 }
 
- export async function BlogPosts() {
+async function BlogPostList() {
   const posts = await getPosts();
-  
-   return (
-     <div>
-       <h1 className="text-3xl font-bold tracking-tight mb-4 ">Latest Posts</h1>
-       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-         {posts.map((post) =><BlogPost data={post} key={post.id}/>)}
-       </div>
-     </div>
-   );
- }
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold tracking-tight mb-4">Latest Posts</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {posts.map((post) => (
+          <BlogPost data={post} key={post.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -3,47 +3,63 @@ import { buttonVariants } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
-const PostDetails = async ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+type Params= Promise<{ id: string}>
+const PostDetails = async ({ params }: { params: Params }) => {
+  const id = (await params).id;
 
-  const data= await getUniquePost(id);
-  
+  const data = await getUniquePost(id);
+
+  if (!data) {
+    return (
+      <div className="text-center text-red-500 font-bold">Post not found.</div>
+    );
+  }
+
   return (
-    <div className='max-w-3xl mx-auto pt-15'>
-      <Link href="/" className={buttonVariants({variant:'secondary'})}>Back to Posts</Link>
-      <h1 className='text-3xl font-bold tracking-tight my-8 text-center'>{data.title}</h1>
-      <div className="flex items-center gap-10 mb-20 justify-center ">
+    <div className="max-w-3xl mx-auto pt-15">
+      <Link href="/" className={buttonVariants({ variant: 'secondary' })}>
+        Back to Posts
+      </Link>
+      <h1 className="text-3xl font-bold tracking-tight my-8 text-center">
+        {data.title}
+      </h1>
+      <div className="flex items-center gap-10 mb-20 justify-center">
         <div className="flex items-center">
           <div className="relative size-8 rounded-full overflow-hidden">
             <Image
               src={data.authorImage}
               alt={data.authorName}
               fill
-              className=" object-cover"
+              className="object-cover"
               priority
-            />      
+            />
           </div>
           <span className="ml-2 text-sm font-medium">{data.authorName}</span>
         </div>
-        <time  className="text-sm text-gray-500">{ new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: 'numeric' }).format(data.createdAt)}</time>
+        <time className="text-sm text-gray-500">
+          {new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          }).format(new Date(data.createdAt))}
+        </time>
       </div>
       <div className="relative overflow-hidden h-[400px] mb-4 rounded-lg">
         <Image
           src={data.imageUrl}
           alt={data.title}
           fill
-          className=" object-cover"
+          className="object-cover"
           priority
         />
       </div>
       <Card>
         <CardContent>{data.content}</CardContent>
       </Card>
-          
     </div>
-  )
-}
+  );
+};
 
-export default PostDetails
+export default PostDetails;
